d525fc1317ac0a8f49578c20b73ace0723ea3c1d
piecesofthought <songki9407@gmail.com>
2026-04-08 15:29:25 +0900

    Update cache files and add new logo formats

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
-const CACHE_NAME = 'allbook-cache-v3';
+const CACHE_NAME = 'allbook-cache-v3'; // v3로 업데이트!
 const urlsToCache = [
   './index.html',
   './manifest.json',
   './AllBook_logo.svg',
+  './AllBook_logo3(512white).svg',
+  './AllBook_logo3(512transp).svg',
   './ewha_font.ttf'
-  './icon2.png',
-  // 추가로 사용하는 로고 svg나 폰트 파일이 있다면 여기에 등록하세요.
 ];
 
-// 설치 및 캐싱
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -18,12 +17,10 @@ self.addEventListener('install', event => {
   );
 });
 
-// 네트워크 요청 가로채기 (오프라인 지원)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // 캐시에 있으면 캐시된 파일 반환, 없으면 네트워크 요청
         return response || fetch(event.request);
       })
   );
