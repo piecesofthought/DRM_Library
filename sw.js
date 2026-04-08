be9a008cbb93229984a4dea177469b1084f02b24
piecesofthought <songki9407@gmail.com>
2026-04-08 10:45:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'allbook-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './AllBook_logo.svg',
+  './ewha_font.ttf'
+  // 추가로 사용하는 로고 svg나 폰트 파일이 있다면 여기에 등록하세요.
+];
+
+// 설치 및 캐싱
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 네트워크 요청 가로채기 (오프라인 지원)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에 있으면 캐시된 파일 반환, 없으면 네트워크 요청
+        return response || fetch(event.request);
+      })
+  );
+});
